1bb18f14223f85b849bfb3342965b4f608c812c6
Bill Parnell <bill.parnell@frequence.com>
2022-03-25 15:41:40 -0700

    first commit

---
+++ learning_templates/basic_app/decor.py
@@ -0,0 +1,32 @@
+
+
+def decorator_with_arguments(function):
+    def wrapper_accepting_arguments(arg1, arg2):
+        print("My arguments are: {0}, {1}".format(arg1,arg2))
+        function(arg1, arg2)
+    return wrapper_accepting_arguments
+
+
+@decorator_with_arguments
+def cities(city_one, city_two):
+    print("Cities I love are {0} and {1}".format(city_one, city_two))
+
+cities("Nairobi", "Accra")
+
+
+
+
+
+
+def plus_one(function):
+    def wrapper(n):
+        print("My arguments are: {0}, {1}".format(arg1,arg2))
+        function(n)
+    return wrapper
+
+
+@decorator_with_arguments
+def cities(city_one, city_two):
+    print("Cities I love are {0} and {1}".format(city_one, city_two))
+
+cities("Nairobi", "Accra")
